b738ce1a40fa3c6c81c22767c7404693326b754a
kowalski100 <ijaz.a100@yahoo.com>
2018-09-23 00:23:57 -0700

    reconfiguration

---
+++ FreeRTOS-Semaphore-Signalling/Objects/port.d
@@ -0,0 +1,12 @@
+.\objects\port.o: ..\FreeRTOS-Source\portable\RVDS\ARM_CM4F\port.c
+.\objects\port.o: ..\FreeRTOS-Source\include\FreeRTOS.h
+.\objects\port.o: e:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
+.\objects\port.o: e:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\port.o: ..\FreeRTOS-Semaphore-Signalling\FreeRTOSConfig.h
+.\objects\port.o: ..\FreeRTOS-Source\include\projdefs.h
+.\objects\port.o: ..\FreeRTOS-Source\include\portable.h
+.\objects\port.o: ..\FreeRTOS-Source\include\deprecated_definitions.h
+.\objects\port.o: ..\FreeRTOS-Source\portable\RVDS\ARM_CM4F\portmacro.h
+.\objects\port.o: ..\FreeRTOS-Source\include\mpu_wrappers.h
+.\objects\port.o: ..\FreeRTOS-Source\include\task.h
+.\objects\port.o: ..\FreeRTOS-Source\include\list.h
